b6a456182e8c9e202a0df76e2b0aa1c69c534a70
Izzet Eray Ertoprak <73610595+Nakzor@users.noreply.github.com>
2020-11-15 23:40:04 +0200

    Changes to Else statement
    Changes to second else statement

--- src/main/java/Fighter/FightingArena.java
+++ src/main/java/Fighter/FightingArena.java
@@ -1,17 +1,25 @@
 package Fighter;
 
+import java.sql.SQLOutput;
+
 public class FightingArena {
     public static void main(String[] args) {
-        Fighter Ryu = new Fighter("Ryu", 200, 20);
-        Fighter Ken = new Fighter("Ken", 150, 30);
-        while (Ryu.getHealth() >= 0 && Ken.getHealth() >= 0) {
+        Fighter Ryu = new Fighter("Ryu", 210, 20);
+        Fighter Ken = new Fighter("Ken", 100, 30);
+        while (Ken.getHealth() > 0 && Ryu.getHealth() > 0) {
             Ryu.Attack(Ryu, Ken);
-            if (Ryu.getHealth() > 0) {
+            if (Ryu.getHealth() > 0 ) {
                 Ken.Attack(Ken, Ryu);
+
+                if (Ken.getHealth() > 0) {
+
+                }else
+                    System.out.println("Winner is " + Ryu.getName());
+
+
+
             }else
                 System.out.println("Winner is " + Ken.getName());
         }
-
-
     }
 }
